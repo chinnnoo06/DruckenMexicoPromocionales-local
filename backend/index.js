4f153596d956848ab4429c57683f4098bbf17ee2
chinnnoo06 <inda5054@gmail.com>
2025-09-27 18:18:37 -0600

    VERSION 1.0.0

--- backend/index.js
+++ backend/index.js
@@ -23,15 +23,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Servir assets estáticos
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-// Rutas
+// Rutas API
 const productRoutes = require("./routes/product");
 const userRoutes = require("./routes/user");
 
 app.use("/api/product", productRoutes);
 app.use("/api/user", userRoutes);
 
+// Servir frontend React en producción
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
+
+// Iniciar servidor
 app.listen(puerto, () => {
     console.log("Servidor corriendo en el puerto:", puerto);
 });
