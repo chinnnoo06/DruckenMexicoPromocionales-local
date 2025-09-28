39c29d2b13b708a1f1ee6c85b74304368b1b54f2
chinnnoo06 <inda5054@gmail.com>
2025-09-28 17:44:09 -0600

    VERSION 1.0.0

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,30 @@
 const connection = require("./database/conection");
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require('cookie-parser');
-const path = require('path');
-require('dotenv').config();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+require("dotenv").config();
 
 console.log("API NODE para DRUCKEN MÉXICO PROMOCIONALES ARRANCADA!!!");
 
+// Conectar BD
 connection();
 
 const app = express();
-const puerto = process.env.PORT;
+const puerto = process.env.PORT || 3000;
 
-// Configurar CORS
-app.use(cors({
-    origin: process.env.URL, // tu frontend
-    credentials: true
-}));
+// Configurar CORS dinámico
+const allowedOrigins = [
+  process.env.URL, // local o producción (según .env)
+  "https://www.drucken.com.mx", // cubrir con www
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 // Body parser
 app.use(express.json());
@@ -24,24 +32,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Servir assets estáticos
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 // Rutas API
 const productRoutes = require("./routes/product");
 const userRoutes = require("./routes/user");
 
-// Primero las rutas API
 app.use("/api/product", productRoutes);
 app.use("/api/user", userRoutes);
 
-// Luego React
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+// Servir frontend (React)
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get(/^\/(?!api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
-
 
 // Iniciar servidor
 app.listen(puerto, () => {
-    console.log("Servidor corriendo en el puerto:", puerto);
+  console.log("Servidor corriendo en el puerto:", puerto);
 });
