63324247842999b70f4e1a58e33b8ae12f3ee54f
chinnnoo06 <inda5054@gmail.com>
2025-11-07 23:47:56 -0600

    VERSION 1.0.0

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -5,7 +5,7 @@ const check = require("../middlewares/auth");
 
 // Definir rutas
 router.get("/prueba-category", check.auth, CategoryController.prueba);
-router.get("/obtener-categorias", check.auth, CategoryController.getCategorys);
+router.get("/obtener-categorias", CategoryController.getCategorys);
 router.post("/crear-categoria", check.auth, CategoryController.addCategory)
 router.post("/actualizar-categoria", check.auth, CategoryController.updateCategory)
 router.delete("/eliminar-categoria/:id", check.auth, CategoryController.remove)
