49baabce1696c8d3472f0e9b7aaf1238a1c0ae2f
chinnnoo06 <inda5054@gmail.com>
2026-01-28 08:12:00 -0600

    changes

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -5,15 +5,13 @@ const corsOptions = {
     // Permitir llamadas sin origin (Postman, curl, pm2 logs, healthchecks)
     if (!origin) return callback(null, true);
 
-    // Permitir SOLO tu frontend
     if (origin === FRONTEND_URL) {
       return callback(null, true);
     }
 
-    // Bloquear cualquier otro origen
     return callback(new Error("Not allowed by CORS"), false);
   },
-  credentials: true, // 🔥 OBLIGATORIO PARA COOKIES
+  credentials: true, 
 };
 
 module.exports = corsOptions;
