8ec84fcfeb5aac7ac6a44d79bff0128b1f6d2122
chinnnoo06 <inda5054@gmail.com>
2026-04-29 21:35:45 -0600

    update

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -30,7 +30,7 @@ export const Order = () => {
       <div className="text-center">
         <h1 className='text-[#9F531B] titulo-seccion font-bold text-[48px] sm:text-[52px] md:text-[55px] lg:text-[60px] mb-6 relative inline-block'>
           Tu pedido
-          <span className="absolute left-1/2 transform -translate-x-1/2 bottom-[-10px] w-24 h-1.5 bg-[#9F531B] rounded-full"></span>
+          <span className="absolute left-1/2 transform -translate-x-1/2 bottom-[-10px] w-24 h-1.5 bg-[rgb(159,83,27)] rounded-full"></span>
         </h1>
         <p className="text-gray-700 text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] font-medium mt-2 block">
           Todo lo que has elegido está aquí, listo para ti.
